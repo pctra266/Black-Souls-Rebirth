a113d23a4e69c23a205e7eb78b31ed87b5ac2d92
Tra Pham <phamtra001@gmail.com>
2025-06-16 23:47:55 +0700

    refactor: update graphic

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,83 +1,135 @@
 using System;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float gravityScale = 5f;
+    [SerializeField] private float jumpHoldGravityMultiplier = 0.5f;
     private Rigidbody2D rb;
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
     private Animator animator;
+    private bool isJumping;
     private bool isGrounded;
+    private bool isAttack;
     private GameManager gameManager;
 
-    private AudioManager audioManager;
+    //private AudioManager audioManager;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         gameManager = FindAnyObjectByType<GameManager>();
-        audioManager = FindAnyObjectByType<AudioManager>();
+        
+        //audioManager = FindAnyObjectByType<AudioManager>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
     {
-        
+        rb.gravityScale = gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameManager.IsGameOver() || gameManager.IsGameWin())
-        {
+        //if (gameManager.IsGameOver() || gameManager.IsGameWin())
+        //{
             
-            return;
-        }
+        //    return;
+        //}
         HandleMovement();
-        HandleJump();
         CheckGround();
+        CheckAttack();
         UploadAnimation();
+        JumpHandle();
+        GravityHandle();
     }
+    private void JumpHandle()
+    {
+        bool isHoldJump = Input.GetButtonDown("Jump");
+
+        if (isHoldJump && isGrounded)
+        {
+            isJumping = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+        bool isReleaseJump = Input.GetButtonUp("Jump"); 
+        if (isReleaseJump)
+        {
+            isJumping = false;
+        }
+
+
+    }
+
+    public void GravityHandle()
+    {
+        if (rb.linearVelocity.y > 0)
+        {
+            if (isJumping)
+            {
+                rb.gravityScale = gravityScale * jumpHoldGravityMultiplier;
+            }
+            else
+            {
+                rb.gravityScale = gravityScale;
+            }
+
+        }
+        else
+        {
+            rb.gravityScale = gravityScale;
+        }
+    }
+
+
+
 
     private void HandleMovement()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-        if(moveInput >= 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else
+        if(moveInput > 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if(moveInput < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+
         }
     }
 
     private void CheckGround()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        
     }
 
-
-    private void HandleJump()
+    private void CheckAttack()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetMouseButtonDown(0)) 
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            audioManager.PlayJumpSound();
+            isAttack = true;
+        }
+        else
+        {
+            isAttack = false;
         }
     }
     private void UploadAnimation()
     {
         bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
-        bool isJumping = !isGrounded;
-        animator.SetBool("isRunning", isRunning);
-        animator.SetBool("isJumping", isJumping);
+        bool isAttacking = isAttack;
+        animator.SetBool("isMoving", isRunning);
+        animator.SetBool("attack", isAttacking);
     }
+
+
 }
